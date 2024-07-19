0fda55ed6a9c2129d23ebef6d2b63c73ff93e462
ET GLap <eglapointe@hotmail.com>
2024-07-19 14:07:03 -0400

    fix: dto to DTO

--- src/main/java/com/rocketFoodDelivery/rocketFood/service/RestaurantService.java
+++ src/main/java/com/rocketFoodDelivery/rocketFood/service/RestaurantService.java
@@ -1,7 +1,7 @@
 package com.rocketFoodDelivery.rocketFood.service;
 
-import com.rocketFoodDelivery.rocketFood.dtos.ApiCreateRestaurantDto;
-import com.rocketFoodDelivery.rocketFood.dtos.ApiRestaurantDto;
+import com.rocketFoodDelivery.rocketFood.dtos.ApiCreateRestaurantDTO;
+import com.rocketFoodDelivery.rocketFood.dtos.ApiRestaurantDTO;
 import com.rocketFoodDelivery.rocketFood.models.Restaurant;
 import com.rocketFoodDelivery.rocketFood.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -55,7 +55,7 @@ public class RestaurantService {
      *
      * @see RestaurantRepository#findRestaurantWithAverageRatingById(int) for the raw query details from the repository.
      */
-    public Optional<ApiRestaurantDto> findRestaurantWithAverageRatingById(int id) {
+    public Optional<ApiRestaurantDTO> findRestaurantWithAverageRatingById(int id) {
         List<Object[]> restaurant = restaurantRepository.findRestaurantWithAverageRatingById(id);
 
         if (!restaurant.isEmpty()) {
@@ -65,7 +65,7 @@ public class RestaurantService {
             int priceRange = (int) row[2];
             double rating = (row[3] != null) ? ((BigDecimal) row[3]).setScale(1, RoundingMode.HALF_UP).doubleValue() : 0.0;
             int roundedRating = (int) Math.ceil(rating);
-            ApiRestaurantDto restaurantDto = new ApiRestaurantDto(restaurantId, name, priceRange, roundedRating);
+            ApiRestaurantDTO restaurantDto = new ApiRestaurantDTO(restaurantId, name, priceRange, roundedRating);
             return Optional.of(restaurantDto);
         } else {
             return Optional.empty();
@@ -80,10 +80,10 @@ public class RestaurantService {
      * @return A list of ApiRestaurantDto objects representing the selected restaurants.
      *         Each object contains the restaurant's ID, name, price range, and a rounded-up average rating.
      */
-    public List<ApiRestaurantDto> findRestaurantsByRatingAndPriceRange(Integer rating, Integer priceRange) {
+    public List<ApiRestaurantDTO> findRestaurantsByRatingAndPriceRange(Integer rating, Integer priceRange) {
         List<Object[]> restaurants = restaurantRepository.findRestaurantsByRatingAndPriceRange(rating, priceRange);
 
-        List<ApiRestaurantDto> restaurantDtos = new ArrayList<>();
+        List<ApiRestaurantDTO> restaurantDtos = new ArrayList<>();
 
             for (Object[] row : restaurants) {
                 int restaurantId = (int) row[0];
@@ -91,7 +91,7 @@ public class RestaurantService {
                 int range = (int) row[2];
                 double avgRating = (row[3] != null) ? ((BigDecimal) row[3]).setScale(1, RoundingMode.HALF_UP).doubleValue() : 0.0;
                 int roundedAvgRating = (int) Math.ceil(avgRating);
-                restaurantDtos.add(new ApiRestaurantDto(restaurantId, name, range, roundedAvgRating));
+                restaurantDtos.add(new ApiRestaurantDTO(restaurantId, name, range, roundedAvgRating));
             }
 
             return restaurantDtos;
@@ -103,11 +103,11 @@ public class RestaurantService {
      * Creates a new restaurant and returns its information.
      *
      * @param restaurant The data for the new restaurant.
-     * @return An Optional containing the created restaurant's information as an ApiCreateRestaurantDto,
+     * @return An Optional containing the created restaurant's information as an ApiCreateRestaurantDTO,
      *         or Optional.empty() if the user with the provided user ID does not exist or if an error occurs during creation.
      */
     @Transactional
-    public Optional<ApiCreateRestaurantDto> createRestaurant(ApiCreateRestaurantDto restaurant) {
+    public Optional<ApiCreateRestaurantDTO> createRestaurant(ApiCreateRestaurantDTO restaurant) {
         return null; // TODO return proper object
     }
 
@@ -131,11 +131,11 @@ public class RestaurantService {
      *
      * @param id                  The ID of the restaurant to update.
      * @param updatedRestaurantDto The updated data for the restaurant.
-     * @return An Optional containing the updated restaurant's information as an ApiCreateRestaurantDto,
+     * @return An Optional containing the updated restaurant's information as an ApiCreateRestaurantDTO,
      *         or Optional.empty() if the restaurant with the specified ID is not found or if an error occurs during the update.
      */
     @Transactional
-    public Optional<ApiCreateRestaurantDto> updateRestaurant(int id, ApiCreateRestaurantDto updatedRestaurantDto) {
+    public Optional<ApiCreateRestaurantDTO> updateRestaurant(int id, ApiCreateRestaurantDTO updatedRestaurantDto) {
         return null; // TODO return proper object
     }
 
