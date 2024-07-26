1b19655033b287ec3736f325fa6c372e9a2fc9f3
Gavriel Rudolph <gavrielmrudolph@gmail.com>
2024-07-26 11:18:36 -0400

    imporvements to authController & Test

--- src/test/java/com/rocketFoodDelivery/rocketFood/api/AuthControllerTest.java
+++ src/test/java/com/rocketFoodDelivery/rocketFood/api/AuthControllerTest.java
@@ -75,7 +75,7 @@ public class AuthControllerTest {
 
     /**
      * Select a random user from the list of users.
-     * 
+     *
      * @return A random UserEntity.
      */
     private UserEntity getRandomUser() {
